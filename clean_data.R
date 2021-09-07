26b5a4db4fdf3a66b9d17f84d479cc3c07e9244d
Laurence-Traynor <alaurencetraynor@blm.gov>
2021-09-07 14:36:12 -0600

    Update
    Added pasture summaries  for ocular estimates, frequency and  Height weight data

--- clean_data.R
+++ clean_data.R
@@ -8,7 +8,9 @@ library(readxl)
 # Set the filepath for data
 wd <- "C:\\Users\\alaurencetraynor\\Documents\\2019\\UI\\Chapter 1\\Data\\"
 
-## Utilization data
+
+############################### Organizing stocking rate data #############################################
+
 # ocular estimates
 ocular <-  read.csv(paste0(wd,"ocular_final_all.csv"))
 #frequency of grazed plants (derived from ocular estimates)
@@ -43,10 +45,10 @@ pp_raw <- merge(x = pp_raw,
 
 observed_pastures_pp <- unique(pp_raw$PastureID_YearOfSurvey)
 
-# just curiopus on the overlap of data collection here
-hw_only_pastures <- observed_pastures_hwla[observed_pastures_hwla %notin% observed_pastures_oc]
-oc_only_pastures <- observed_pastures_oc[observed_pastures_oc %notin% observed_pastures_hwla]
-pp_only_pastures <- observed_pastures_pp[observed_pastures_pp %notin% observed_pastures_hwla & observed_pastures_pp %notin% observed_pastures_oc]
+# just curious on the overlap of data collection here
+hw_only_pastures <- observed_pastures_hwla[observed_pastures_hwla %notin% observed_pastures_oc] # 16 unique pastures with only height-weight/landscape appearance data
+oc_only_pastures <- observed_pastures_oc[observed_pastures_oc %notin% observed_pastures_hwla] #7 unique pastures with only ocular/frequency data
+pp_only_pastures <- observed_pastures_pp[observed_pastures_pp %notin% observed_pastures_hwla & observed_pastures_pp %notin% observed_pastures_oc] # none
 
 # pull them all together so we can use them to subset stocking rate data
 observed_pastures <- unique(c(observed_pastures_hwla, observed_pastures_oc, observed_pastures_pp))
@@ -60,11 +62,11 @@ stocking_rate_observed <- stocking_rate[stocking_rate$PastureID %in% observed_pa
 # we can pull this out once the utilization data is summarised to pasture
 
 # Calculating AUMs
-stocking_rate_observed$MonthsGrazed <- difftime(units="days",stocking_rate_observed$DateOut,stocking_rate_observed$DateIn)/31
+stocking_rate_observed$MonthsGrazed <- difftime(units="days",stocking_rate_observed$DateOut,stocking_rate_observed$DateIn)/30 # Changed this to 30 based formula from VJ 
 
 # every where theres a monthsgrazed of 0 we are rounding up to 1 day (1/31) to capture that day of grazing
 # need to format date/time cols appropriately
-stocking_rate_observed$MonthsGrazed[stocking_rate_observed$MonthsGrazed == 0] <- 1/31
+stocking_rate_observed$MonthsGrazed[stocking_rate_observed$MonthsGrazed == 0] <- 1/30
 
 stocking_rate_observed$DateIn <- as.Date(stocking_rate_observed$DateIn)
 stocking_rate_observed$DateOut <- as.Date(stocking_rate_observed$DateOut)
@@ -78,56 +80,90 @@ stocking_rate_observed$AUM <- stocking_rate_observed$NumberOfAnimals*stocking_ra
 stocking_rate_observed <- stocking_rate_observed %>%
   mutate(AUM = ifelse(Rested == "Yes", 0, AUM))
 
-# create better palette
-library(RColorBrewer)
-# Define the number of colors you want
-nb.cols <- length(unique(stocking_rate_observed$PastureID))
-mycolors <- colorRampPalette(brewer.pal(8, "Set2"))(nb.cols)
+####################################################################################################################
+# # these plots give a visual of when the spring/fall grazing cut off was - there doesnt really seem to be any..
+# # create better palette
+# library(RColorBrewer)
+# # Define the number of colors you want
+# nb.cols <- length(unique(stocking_rate_observed$PastureID))
+# mycolors <- colorRampPalette(brewer.pal(8, "Set2"))(nb.cols)
+# 
+# # fix structure a little
+# stocking_rate_observed$ID <- as.factor(stocking_rate_observed$ID)
+# stocking_rate_observed$Year <- as.factor(stocking_rate_observed$Year)
+# stocking_rate_observed$Site <- as.factor(stocking_rate_observed$Site)
+# stocking_rate_observed$PastureID <- as.factor(stocking_rate_observed$PastureID)
+# 
+# # lets do a quick plot here to look at whats going on
+# # size of rectangle is basically AUMs
+# for(i in c("BIBU", "BRBE", "JISA", "PAVA", "SHCR")){
+#   data <- stocking_rate_observed[stocking_rate_observed$Site == i & !is.na(stocking_rate_observed$DateIn),]
+#   
+#   ggplot(data = data, aes(fill = PastureID))+
+#     geom_rect(aes(xmin = DateIn, xmax = DateOut, ymin = 0, ymax = NumberOfAnimals))+
+#     facet_wrap(.~Year)+
+#     scale_x_date(date_labels = "%d-%m", date_breaks = "2 week")+
+#     theme(axis.text.x  = element_text(angle = 60, hjust =1), legend.position = "")+
+#     scale_fill_manual(name = "Pasture ID", values = mycolors)+
+#     labs(y = "Number of Animals", x = "Day-Month")
+#   
+#   ggsave(filename = paste0("stocking rate data ",i, ".jpg"), dpi = 500, device = "jpg", path = wd, width = 20, height = 16, units = "in")
+#   
+# }
+########################################################################################################################
 
-# fix structure a little
-stocking_rate_observed$ID <- as.factor(stocking_rate_observed$ID)
-stocking_rate_observed$Year <- as.factor(stocking_rate_observed$Year)
-stocking_rate_observed$Site <- as.factor(stocking_rate_observed$Site)
-stocking_rate_observed$PastureID <- as.factor(stocking_rate_observed$PastureID)
+# Import pasture information
+#tbl_pastures <- readxl::read_xlsx(path =paste0(wd, "tbl_PastureNames.xlsx"))
+tbl_pastures_new <- readxl::read_xlsx(path = paste0(wd, "Temp_PasturesNew.xlsx"))
 
-# lets do a quick plot here to look at whats going on
-# size of rectangle is basically AUMs
-for(i in c("BIBU", "BRBE", "JISA", "PAVA", "SHCR")){
-  data <- stocking_rate_observed[stocking_rate_observed$Site == i & !is.na(stocking_rate_observed$DateIn),]
-  
-  ggplot(data = data, aes(fill = PastureID))+
-    geom_rect(aes(xmin = DateIn, xmax = DateOut, ymin = 0, ymax = NumberOfAnimals))+
-    facet_wrap(.~Year)+
-    scale_x_date(date_labels = "%d-%m", date_breaks = "2 week")+
-    theme(axis.text.x  = element_text(angle = 60, hjust =1), legend.position = "")+
-    scale_fill_manual(name = "Pasture ID", values = mycolors)+
-    labs(y = "Number of Animals", x = "Day-Month")
-  
-  ggsave(filename = paste0("stocking rate data ",i, ".jpg"), dpi = 500, device = "jpg", path = wd, width = 20, height = 16, units = "in")
-  
-}
-# these plots also give a visual of when the spring/fall grazing cut off was - there doesnt really seem to be any..
+# compare with this grazing data
+#grazing_events <- readxl::read_xlsx(path = paste0(wd, "qry_GrazeDataByGrazeEvent_GrazData.xlsx"))
 
+#grazing_events$AUMs <- as.character(grazing_events$AUMs)
 
-# Summarising to pasture and year to sum individual grazing events
+# check_aums <-  merge(x = grazing_events,
+#                      y = stocking_rate_pasture,
+#                      by = c("PastureID", "Year"))
+
+# Summarising utilization to pasture and year to sum individual grazing events
 # But we may also want to capture spring vs fall grazing here so splitting up to spring AUMs and total AUMs
-# the spring cut off date is June 15th asccording to teh G and G report
+# the spring cut off date is June 15th asccording to teh G and G report--
 
 stocking_rate_pasture <-  stocking_rate_observed %>%
   group_by(Year, PastureID, Site) %>%
   summarise(AUMs = sum(AUM))
 
+stocking_rate_pasture <- merge(x = stocking_rate_pasture,
+                               y = tbl_pastures_new,
+                               by = "PastureID",
+                               all.x = TRUE,
+                               all.y = FALSE)
+
+
+# specify treatment type based on treatment start year
+stocking_rate_pasture$Treatment[stocking_rate_pasture$Treatment != "Non-Treatment" & as.numeric(as.character(stocking_rate_pasture$Year)) < stocking_rate_pasture$YearStudyStart] <- "Pre-Treatment"
+     
+# calc AUMs/hectare
+stocking_rate_pasture$AUMha <- stocking_rate_pasture$AUMs / stocking_rate_pasture$Area_ha
+
+# select only useful columns:
+
+stocking_rate_pasture <- stocking_rate_pasture %>%
+  select(PastureID,
+         Year,
+         Treatment,
+         Site.x,
+         AUMs,
+         AUMha) %>%
+  rename(Site = Site.x)
+
+# export to csv
+write.csv(x = stocking_rate_pasture, file = paste0(wd, "final_stocking_rate_observed_pastures.csv"))
+
+
+#####################################################################################################################
+
 ### Ocular Estimates
-# joining to stocking rate data and filtering based on grazing events
-ocular <- base::merge(x=ocular, y=stocking_rate, all.x = TRUE, all.y =FALSE, by.x = c("New_YearOfSurveyID","Year"), by.y = c("PastureID", "Year"))
-
-ocular <- ocular %>%
-  dplyr::select(-c(StudyPasture_Yes_No,YearStudyStart,YearStudyEnd, HomeState))
-
-# ascertaining actual grazing treatment
-#ocular$grazed <- ifelse(ocular$Treatment == "Spring Odd Years" & ocular$Year == "2015" | ocular$Treatment == "Spring Odd Years" & ocular$Year == "2017", ocular$grazed <- "Yes", ifelse(ocular$Treatment == "Spring Even Years" & ocular$Year == "2016" | ocular$Treatment == "Spring Even Years" & ocular$Year == "2018", ocular$grazed <- "Yes", ifelse(ocular$Treatment == "Spring and Fall", ocular$grazed <- "Yes","No")))
-
-ocular <- filter(ocular, AUMs != 0)
 
 ocular$SiteMove <- ifelse(as.character(ocular$Site) == as.character(ocular$Original_Site_Assignment), ocular$SiteMove <- "No",ocular$SiteMove <-  "Yes")
 
@@ -141,34 +177,65 @@ ocular<- dplyr::select(ocular, -c(Original_Site_Assignment,
                                   PastureID_YearOfSurvey,
                                   StartTime,
                                   EndTime,
-                                  ALLOT_NAME,
-                                  PAST_NAME,
-                                  ADMIN_ORG_,
-                                  Shape_Length,
-                                  Shape_Area,
-                                  Grazed2018,
-                                  Pst_2015Earlier,
-                                  Pst_2016,
-                                  Pst_2018,
-                                  Pst_2019,
-                                  Comment,
-                                  Area_ha))
+                                  Comment))
 
+# convert all charcter variables to factors for the modelling
+ocular[sapply(ocular, is.character)] <- lapply(ocular[sapply(ocular, is.character)], 
+                                       as.factor)
+
+# same with pasture ids and year
 ocular$ObsID1 <- as.factor(ocular$ObsID1)
-
 ocular$ObsID2 <- as.factor(ocular$ObsID2)
-
 ocular$Year <- as.factor(ocular$Year)
-
 ocular$SiteMove <- factor(ocular$SiteMove, levels=c("Yes","No"))
+ocular$New_YearOfSurveyID <- as.factor(ocular$New_YearOfSurveyID)
 
-# need to create AUM/area with correct pasture area - join new oasture id to pasture table
-pastures <- sqlFetch(con1, "Temp_PasturesNew")[,c(1,8)]
+# Joining ocular and frequency data (since they were collected at the same locations and by the same people)
+util_join <- merge(x = ocular,
+                   y = freq[,c("PointID", "percent_grazed")], 
+                   by = "PointID",
+                   all.x = TRUE,
+                   all.y = FALSE)
 
-ocular <- merge(x = ocular, y = pastures, by.x = "New_YearOfSurveyID", by.y = "PastureID", all.x = TRUE, all.y = FALSE)
+write.csv(x = util_join, file = paste0(wd, "final_ocular_frequency_plot.csv"))
 
-ocular$AUMha <-  ocular$AUMs/ocular$Area_ha
+## Summarise to pasture
+ocular_freq_pasture <- util_join %>%
+  rename(ocular = AvgRemoved, frequency = percent_grazed) %>%
+  select(TheorID, Year, New_YearOfSurveyID, Site, ocular, frequency) %>%
+  pivot_longer(cols = c(ocular, frequency), names_to = "method", values_to = "Utilization") %>%
+  group_by(Site, Year, New_YearOfSurveyID, method) %>%
+  summarise(mean_utilization = mean(Utilization),
+            sd_utilization = sd(Utilization),
+            var_utilization = var(Utilization),
+            n = n())
 
-stocking_rate <- dplyr::distinct(ocular[,c(1,2,31)])
+# these are for sure not normally distributed - use beta distribution?
 
-util_join <- left_join(ocular,freq, by = "PointID")
+# joining to stocking rate and pasture data 
+ocular_freq_pasture <- merge(x=ocular_freq_pasture,
+                      y=stocking_rate_pasture,
+                      all.x = TRUE,
+                      all.y =FALSE,
+                      by.x = c("New_YearOfSurveyID","Year","Site"),
+                      by.y = c("PastureID", "Year", 'Site'))
+
+# Filter to pastures that were grazed
+ocular_freq_pasture_grazed <- filter(ocular_freq_pasture, AUMs > 0)
+
+write.csv(x = ocular_freq_pasture_grazed, file = paste0(wd, "final_ocular_freq_pasture.csv"))
+
+### Height weight / landscape appearance
+hw_la$SiteMove <- ifelse(as.character(hw_la$Site) == as.character(hw_la$Original_Site_Assignment), hw_la$SiteMove <- "No",hw_la$SiteMove <-  "Yes")
+
+# convert all charcter variables to factors for the modelling
+hw_la[sapply(hw_la, is.character)] <- lapply(hw_la[sapply(hw_la, is.character)], 
+                                               as.factor)
+
+# same with pasture ids and year
+hw_la$Year <- as.factor(hw_la$Year)
+hw_la$SiteMove <- factor(hw_la$SiteMove, levels=c("Yes","No"))
+hw_la$PastureID_YearOfSurvey <- as.factor(hw_la$PastureID_YearOfSurvey)
+
+# Summarise to pasture 
+
